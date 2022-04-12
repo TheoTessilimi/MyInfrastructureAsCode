34fa63b51acb6d84ccf0373628e6b12fd5d7413c
Tessilimi Theo <dafraise6@gmail.com>
2022-04-12 21:08:50 +0200

    📦 NEW: First commit of my infrastructure as code

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description = "AWS acces key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
